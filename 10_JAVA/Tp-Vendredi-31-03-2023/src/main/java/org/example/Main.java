870958c850ff79bd59d3d92bfbe1533fb298dae8
Belhassen <bbeague@gmail.com>
2023-03-31 11:48:52 +0200

    add Tableaux

--- 10_JAVA/Tp-Vendredi-31-03-2023/src/main/java/org/example/Main.java
+++ 10_JAVA/Tp-Vendredi-31-03-2023/src/main/java/org/example/Main.java
@@ -1,10 +1,17 @@
 package org.example;
 
 import org.example.ex1.Ex1;
+import org.example.ex10Tableaux5.Ex10;
+import org.example.ex11Tableaux6.Ex11;
+import org.example.ex12Tableaux7.Ex12;
 import org.example.ex2.Ex2;
 import org.example.ex3.Ex3;
 import org.example.ex4.Ex4;
 import org.example.ex5.Ex5;
+import org.example.ex6Tableaux1.Ex6;
+import org.example.ex7Tableaux2.Ex7;
+import org.example.ex8Tableaux3.Ex8;
+import org.example.ex9Tableaux4.Ex9;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,7 +19,14 @@ public class Main {
 //        Ex2.nombreCaract();
 //        Ex3.motDePasse();
 //        Ex4.temperature();
-        Ex5.moyenne();
+//        Ex5.moyenne();
+//        Ex6.tableau1();
+//        Ex7.Tableau2();
+//        Ex8.Tableau3();
+//        Ex9.Tableau4();
+//        Ex10.Tableau5();
+//        Ex11.Tableau6();
+        Ex12.Tableau7();
 
     }
 }
